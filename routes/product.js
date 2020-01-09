0dc4daef769c992e22bb392d79db28dfc6cc53a1
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2020-01-10 04:39:54 +0700

    sua loi rount product

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var productController =require('../controllers/productController');
 //thông tin sản phẩm
-router.get('/:id', productController.productInfo);
 
 router.post('/add-product', productController.postAddProduct);
 router.get('/add-product', productController.getAddProduct);
 
 //lấy thông tin người bán
 router.post('/seller-contact',productController.sellerContact);
+router.get('/:id', productController.productInfo);
 
 module.exports = router;
